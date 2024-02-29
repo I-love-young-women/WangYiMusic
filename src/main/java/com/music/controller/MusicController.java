bc004a193970bde6f9ed8104f93212d0f1200253
zc <ziji9362667@163.com>
2024-02-29 19:43:52 +0800

    MusicController 以及WebSupport

--- src/main/java/com/music/controller/MusicController.java
+++ src/main/java/com/music/controller/MusicController.java
@@ -1,9 +1,16 @@
 package com.music.controller;
 
 
+import com.music.pojo.Music;
+import com.music.service.IMusicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -15,6 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/music")
+@CrossOrigin
 public class MusicController {
+    @Autowired
+    private IMusicService service;
+
+
+
+    @GetMapping("/getAll")
+    public List<Music> getAll(){
+        return service.list();
+    }
 
 }
