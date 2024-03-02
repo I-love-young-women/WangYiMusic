eb9b499b2d3bb01feac0d9e21d2c1350e096e12e
zc <ziji9362667@163.com>
2024-03-02 13:06:15 +0800

    完成分页已经专辑图片加载

--- src/main/java/com/music/controller/MusicController.java
+++ src/main/java/com/music/controller/MusicController.java
@@ -1,14 +1,24 @@
 package com.music.controller;
 
 
+import com.baomidou.mybatisplus.extension.toolkit.Db;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.mpatric.mp3agic.*;
 import com.music.pojo.Music;
+import com.music.pojo.Result;
 import com.music.service.IMusicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 import java.util.List;
 
@@ -27,11 +37,25 @@ public class MusicController {
     @Autowired
     private IMusicService service;
 
-
-
-    @GetMapping("/getAll")
-    public List<Music> getAll(){
-        return service.list();
+    @GetMapping("/getAll/{pageNum}/{size}")
+    public Result getAll(@PathVariable Integer pageNum, @PathVariable Integer size) {
+        PageHelper.startPage(pageNum, size);
+        List<Music> list = service.list();
+        PageInfo<Music> pageInfo = new PageInfo<>(list, 3);
+        return new Result(200, pageInfo, "获取歌单成功");
     }
 
+    @GetMapping("/getCover/{id}")
+    public Result getCover(@PathVariable Integer id) throws InvalidDataException, IOException, UnsupportedTagException {
+        Music music = Db.getById(id, Music.class);
+        return service.getCover(music);
+    }
 }
+
+
+
+
+
+
+
+
